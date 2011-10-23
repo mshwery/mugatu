85c5ba311230a83cc2a124f37a200750087c810d
Matt Shwery <mshwery@gmail.com>
2011-10-23 14:25:34 -0500

    added tests for title

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_title = "Mugatu"
+  end
 
   describe "GET 'home'" do
+
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => @base_title + " | Home")
+    end
+
   end
 
   describe "GET 'support'" do
@@ -14,12 +26,22 @@ describe PagesController do
       get 'support'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'support'
+      response.should have_selector("title", :content => @base_title + " | Support")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
