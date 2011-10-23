de9b08daa3280d8662c5f59be0e12386bbaf638f
Matt Shwery <mshwery@gmail.com>
2011-10-23 14:16:30 -0500

    added Pages controller and views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def support
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Mugatu::Application.routes.draw do
 
+  get "pages/home"
+
+  get "pages/support"
+
+  get "pages/contact"
+
 end
