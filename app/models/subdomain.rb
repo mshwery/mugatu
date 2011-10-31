22790db4423564cc4f781cd4d94cb52c9cb82b7d
Matt Shwery <mshwery@gmail.com>
2011-10-31 08:54:59 -0500

    annotated all models

--- app/models/subdomain.rb
+++ app/models/subdomain.rb
@@ -1,6 +1,16 @@
+# == Schema Information
+#
+# Table name: subdomains
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Subdomain < ActiveRecord::Base
   has_many :users, :dependent => :destroy
 
-  validates :name, :presence => true
+  validates :name, :presence => true,
                     :uniqueness => { :case_sensitive => false }
 end
--- spec/models/subdomain_spec.rb
+++ spec/models/subdomain_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: subdomains
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Subdomain do
